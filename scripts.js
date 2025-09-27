827798fb8de9224ed02e1a583725debba8cb1587
OwenBahen <86366947+OwenBahen@users.noreply.github.com>
2025-09-26 22:06:28 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// --- Seasonal Emojis ---
+const seasons = [
+  { name:'Spring', gradient:['#6DBE45','#A7D49B'], elements:['🌸','🍃','🐦','☘️','🌼','🐝'] },
+  { name:'Summer', gradient:['#3E8E41','#82C784'], elements:['☀️','🌻','🌴','🍹','🐞','🦋'] },
+  { name:'Fall', gradient:['#A0522D
